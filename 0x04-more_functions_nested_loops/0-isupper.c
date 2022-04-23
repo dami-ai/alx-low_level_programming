8558f322491db9e4e092c95529c007ce19a5e040
Ayinde Yetunde Idowu <idowu4028@gmail.com>
2022-04-23 09:42:08 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * _isupper  - checks upper case
+ * @c: prints int
+ * Return: gives 1 or 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (!);
+}
+
+else
+{
+return (0);
+}
+
+}
