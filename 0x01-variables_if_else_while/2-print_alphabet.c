acb8916b8f28acd0ce43ddd70956121337481f99
Ayinde Yetunde Idowu <idowu4028@gmail.com>
2022-04-24 07:25:25 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Deascription: Print the lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 26; c++)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
