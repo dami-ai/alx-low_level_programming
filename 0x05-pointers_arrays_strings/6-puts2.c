1504e5f5a4cd081eea90caadeec7da6216721d34
Ayinde Yetunde Idowu <idowu4028@gmail.com>
2022-04-27 06:44:24 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer char
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i, x;
+i = 0;
+x = 0;
+while (str[i++])
+{
+x++;
+}
+for (i = 0; i < x; i += 2)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
+}
